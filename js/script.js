79c58606783911bc93d3d0c8369aba83dbe422b9
Karen Justo <40191282+JR-Karen9@users.noreply.github.com>
2018-07-29 03:50:34 -0300

    Improve the code and make the console write the score

--- js/script.js
+++ js/script.js
@@ -1,69 +1,59 @@
 function computerPlay() {
     let random = Math.floor(Math.random()*3);
-    if(random == 1) return "Rock";
-    if(random == 2) return "Paper";
-    else return "Scissors";
+    if(random == 1) return "rock";
+    if(random == 2) return "paper";
+    else return "scissors";
 }
 
 function playRound(playerSelection, computerSelection){
 
-    if(!playerSelection.localeCompare(computerSelection)) return 0;
-    if(!playerSelection.localeCompare('rock')){
-        if(!computerSelection.localeCompare('scissors')){
-            return 1;
-        }
-        else{
-            return -1;
-        }
-    }
-    else if(!playerSelection.localeCompare('scissors')){
-        if(!computerSelection.localeCompare('paper')){
-            return 1;
-        }
-        else{
-            return -1;
-        }
-    }
-    else if(!playerSelection.localeCompare('paper')){
-        if(!computerSelection.localeCompare('rock')){
-            return 1;
-        }
-        else{
-            return -1;
-        }
-    }
-    else{
-        return 2;
-    }
+    if(!playerSelection.localeCompare(computerSelection)) return 'draw';
+    if(!playerSelection.localeCompare('rock'))
+        return (!computerSelection.localeCompare('scissors'));
+    else if(!playerSelection.localeCompare('scissors'))
+        return (!computerSelection.localeCompare('paper'));
+    else if(!playerSelection.localeCompare('paper'))
+        return (!computerSelection.localeCompare('rock'));
+    else
+        return 'wrong';
 }
 
 function message(value, playerSelection, computerSelection){
-    if(value==0) return "Draw, try again";
-    if(value==1) return `You win! ${playerSelection} beats ${computerSelection}`;
-    else if(value==2) return "You didn't choose a valid option";
-    else return `You lose, ${computerSelection} beats ${playerSelection}`;
+    if(value=='draw') return "Draw, try again";
+    if(value=='wrong') return "You didn't choose a valid option";
+    if(value==true) return `You win! ${playerSelection} beats ${computerSelection}`;
+    return `You lose, ${computerSelection} beats ${playerSelection}`;
 }
 
 function game(){
     let playerScore = 0;
     let computerScore = 0;
     for(let i=0; i<5; i++){
-        let playerSelection = prompt("Choose one")
-        let player = playerSelection.toLowerCase();
-        let computerSelection = computerPlay();
-        let computer = computerSelection.toLowerCase();
-        let value = playRound(player, computer);
-    
-        if(value==1){
-            playerScore+=1;
-        } 
-        else if(value==-1){
-            computerScore+=1;
+        
+        let playerSelection = prompt("Choose Rock, Paper or Scissors");
+        
+        if(playerSelection!==null){
+            let player = playerSelection.toLowerCase();
+            let computerSelection = computerPlay();
+            let value = playRound(player, computerSelection);
+        
+            if(value==true){
+                playerScore+=1;
+            } 
+            else if(value==false){
+                computerScore+=1;
+            }
+            else if(value=='wrong')
+                i--;
+
+            console.log(message(value, playerSelection, computerSelection));
+            console.log('SCORE - Player: ' + playerScore + ', Computer: ' + computerScore);
         }
 
-        console.log(message(value, playerSelection, computerSelection));  
     }
     if(playerScore>computerScore) console.log("You've won!");
     else if(playerScore==computerScore) console.log("It's a tie");
     else console.log("You've lost :(");
 }
+
+game();
